6fa633d4b0e75fe8f165d8b8cfaa31458176f9f6
Oralia Rivera <oralia_rivera31@hotmail.com>
2024-07-18 00:49:48 -0600

    added new changes

--- scripts/script.js
+++ scripts/script.js
@@ -1,50 +1,57 @@
-var resultado = document.getElementById('result').value;
+var resultado = document.getElementById("result").value;
 
 function fillIn(valueButton) {
-    var aux = document.getElementById("result").value;
-    if (aux === '0') {
-        document.getElementById("result").value = valueButton
-    } else {
-        document.getElementById("result").value = aux + valueButton;
-    }
-
+  var aux = document.getElementById("result").value;
+  if (aux === "0") {
+    document.getElementById("result").value = valueButton;
+  } else {
+    document.getElementById("result").value = aux + valueButton;
+  }
 }
 
 //Función para limpiar el resultado
 function clearNumbers() {
-    document.getElementById("result").value = 0;
+  document.getElementById("result").value = 0;
 }
 
-// Función para borar digito por digito
+//Función para borar digito por digito
 function deleteNumbers() {
-    var del = document.getElementById("result").value;
-    document.getElementById("result").value = del.length;
-    document.getElementById("result").value = del.substr(0, del.length - 1);
-}
+  var resultElement = document.getElementById("result");
+  var del = resultElement.value;
 
+  //Borrar el ultimo digito
+  if (del.length > 0) {
+    resultElement.value = del.substr(0, del.length - 1);
+  }
+
+  //Si no queda ningun número, establecer el valor a 0
+  if (resultElement.value.length === 0) {
+    resultElement.value = 0;
+  }
+}
 
 function point() {
-    document.getElementById("result").value += ".";
+  document.getElementById("result").value += ".";
 }
 
 // Funçión para alterar un número positivo ou negativo
 function maisMenos() {
-    if (document.getElementById("result").value == "") {
-        document.getElementById("result").value = "-";
-    } else {
-        document.getElementById("result").value = "";
-    }
+  if (document.getElementById("result").value == "") {
+    document.getElementById("result").value = "-";
+  } else {
+    document.getElementById("result").value = "";
+  }
 }
 
 function calculate() {
-    const calculateResult = eval(document.getElementById("result").value);
-    document.getElementById("result").value = calculateResult;
+  const calculateResult = eval(document.getElementById("result").value);
+  document.getElementById("result").value = calculateResult;
 }
 
 function porcentagem() {
-    valor2 = parseFloat(document.getElementById("result").value);
-    if (operador == "*") {
-        resultado = (valor1 / 100) * valor2;
-    }
-    document.getElementById("result").value = resultado;
+  valor2 = parseFloat(document.getElementById("result").value);
+  if (operador == "*") {
+    resultado = (valor1 / 100) * valor2;
+  }
+  document.getElementById("result").value = resultado;
 }
